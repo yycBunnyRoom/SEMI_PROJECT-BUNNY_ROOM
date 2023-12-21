08fc59d90b0a6e8c3b20c772410ce5c4babfcc4a
isac7722 <isac7722@gmail.com>
2023-12-21 21:22:47 +0900

    static에 js 폴더 만들어서 js 파일 따로 정리

--- bunny-room/src/main/resources/static/js/main.js
+++ bunny-room/src/main/resources/static/js/main.js
@@ -1,8 +1,24 @@
-
-
 // JavaScript 변수 정의
-var myString1 = "1312312312312321321";
+var myString = userAuth+" "+userNickname+"님";
 
 // 변수 값을 <p> 태그 안에 넣기
-document.getElementById("test").innerHTML = myString1;
-document.getElementById("test").style.display = 'none';
+document.getElementById("output").innerHTML = myString;
+
+
+<!--    로그인 버튼 관련 JS-->
+if (userAuth == null){
+    document.getElementById("btn_login").style.display = 'block';
+    document.getElementById("btn_logout").style.display = 'none';
+}
+if (userAuth)
+{
+    document.getElementById("btn_login").style.display = 'none';
+
+    if (userAuth == "ADMIN"){
+        document.getElementById("btn_adminPage").style.display = 'block';
+        document.getElementById("btn_hostMainView").style.display = 'block';
+    }
+    else if (userAuth == "HOST"){
+        document.getElementById("btn_hostMainView").style.display = 'block';
+    }
+}
