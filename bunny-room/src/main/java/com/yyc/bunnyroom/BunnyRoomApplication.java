994ad976bc9b66182b21cd78680f35115c517d27
ClownYH <qwer1745@gmail.com>
2023-12-29 14:26:57 +0900

    bug test

--- bunny-room/src/main/java/com/yyc/bunnyroom/BunnyRoomApplication.java
+++ bunny-room/src/main/java/com/yyc/bunnyroom/BunnyRoomApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BunnyRoomApplication {
 
+    // commit test
     public static void main(String[] args) {
         SpringApplication.run(BunnyRoomApplication.class, args);
     }
