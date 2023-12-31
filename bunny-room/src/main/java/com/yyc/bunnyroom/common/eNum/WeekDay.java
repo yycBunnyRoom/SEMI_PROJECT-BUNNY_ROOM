b2ca5120b02a06ff567cb132c7594398848d65ec
isac7722 <isac7722@gmail.com>
2024-01-01 02:50:46 +0900

    common Weekday enum에 값을 추가 (휴무 선택에 있어 필요함)

--- bunny-room/src/main/java/com/yyc/bunnyroom/common/eNum/WeekDay.java
+++ bunny-room/src/main/java/com/yyc/bunnyroom/common/eNum/WeekDay.java
@@ -1,26 +1,28 @@
 package com.yyc.bunnyroom.common.eNum;
 
 public enum WeekDay {
-    MONDAY("월"),
-    TUESDAY("화"),
-    WEDNESDAY("수"),
-    THURSDAY("목"),
-    FRIDAY("금"),
-    SATURDAY("토"),
-    SUNDAY("일");
+    MONDAY(1, "월"),
+    TUESDAY(2, "화"),
+    WEDNESDAY(3, "수"),
+    THURSDAY(4, "목"),
+    FRIDAY(5, "금"),
+    SATURDAY(6, "토"),
+    SUNDAY(0, "일");
 
+    private final int value; // int 값을 저장할 필드 추가
     private final String description;
 
+    WeekDay(int value, String description) {
+        this.value = value;
+        this.description = description;
+    }
 
-    WeekDay(String description){this.description = description;}
+    public int getValue() {
+        return value;
+    }
 
     public String getDescription() {
         return description;
-    }
-
-    @Override
-    public String toString() {
-        return super.toString();
     }
 
 }
