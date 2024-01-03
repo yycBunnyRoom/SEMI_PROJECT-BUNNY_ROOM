1d3aac00f2e4f9aa4daa5b8410e04bd2b15b800a
ClownYH <qwer1745@gmail.com>
2024-01-03 16:11:51 +0900

    관리자 회원 관리에서 날짜 포맷팅, 회원 탈퇴 사유 추가

--- bunny-room/src/main/java/com/yyc/bunnyroom/admin/model/dto/MemberDTO.java
+++ bunny-room/src/main/java/com/yyc/bunnyroom/admin/model/dto/MemberDTO.java
@@ -1,7 +1,10 @@
 package com.yyc.bunnyroom.admin.model.dto;
 
+import lombok.Data;
+
 import java.time.ZonedDateTime;
 
+@Data
 public class MemberDTO {
 
     private int userNo;
@@ -20,95 +23,6 @@ public class MemberDTO {
 
     private ZonedDateTime userUpdateDate;
 
-    public MemberDTO() {
-    }
+    private String withdrawReason;
 
-    public MemberDTO(int userNo, String email, String nickname, String phone, String status, String auth, ZonedDateTime userRegistDate, ZonedDateTime userUpdateDate) {
-        this.userNo = userNo;
-        this.email = email;
-        this.nickname = nickname;
-        this.phone = phone;
-        this.status = status;
-        this.auth = auth;
-        this.userRegistDate = userRegistDate;
-        this.userUpdateDate = userUpdateDate;
-    }
-
-    public int getUserNo() {
-        return userNo;
-    }
-
-    public void setUserNo(int userNo) {
-        this.userNo = userNo;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getAuth() {
-        return auth;
-    }
-
-    public void setAuth(String auth) {
-        this.auth = auth;
-    }
-
-    public ZonedDateTime getUserRegistDate() {
-        return userRegistDate;
-    }
-
-    public void setUserRegistDate(ZonedDateTime userRegistDate) {
-        this.userRegistDate = userRegistDate;
-    }
-
-    public ZonedDateTime getUserUpdateDate() {
-        return userUpdateDate;
-    }
-
-    public void setUserUpdateDate(ZonedDateTime userUpdateDate) {
-        this.userUpdateDate = userUpdateDate;
-    }
-
-    @Override
-    public String toString() {
-        return "MemberDTO{" +
-                "userNo=" + userNo +
-                ", email='" + email + '\'' +
-                ", nickname='" + nickname + '\'' +
-                ", phone='" + phone + '\'' +
-                ", status='" + status + '\'' +
-                ", auth='" + auth + '\'' +
-                ", userRegistDate=" + userRegistDate +
-                ", userUpdateDate=" + userUpdateDate +
-                '}';
-    }
 }
