81d3120ba9e5c6dcbcfb0dd74610655191ae220f
isac7722 <isac7722@gmail.com>
2024-01-10 18:23:04 +0900

    roomDetail fixed

--- bunny-room/src/main/resources/static/js/roomRegister/detail/roomDetail.js
+++ bunny-room/src/main/resources/static/js/roomRegister/detail/roomDetail.js
@@ -10,7 +10,36 @@ function createAppliedOptionsButtons() {
 
 window.onload = () => {
     createAppliedOptionsButtons();
+    downloadImage(roomNo);
 };
+
+
+function triggerFileInput() {
+    const fileInput = document.getElementById('imageIdx');
+    if (fileInput) {
+        fileInput.click(); // 파일 업로더 실행
+    }
+}
+
+
+function handleFileChange(event) {
+    const fileInput = document.getElementById('imageIdx');
+    // 선택된 파일이 있으면 업로드 로직을 실행할 수 있습니다.
+    if (fileInput && fileInput.files.length > 0) {
+        // 여기에 파일 업로드 로직을 추가하세요
+        const selectedFile = fileInput.files[0];
+        console.log('선택된 파일:', selectedFile);
+        uploadImage();
+    }
+}
+
+
+
+
+
+
+
+
 
 
 
@@ -53,4 +82,26 @@ function uploadImage() {
     } else {
         alert("이미지를 등록하고 있습니다.")
     }
+}
+
+
+
+
+function downloadImage(roomNo) {
+
+    fetch(`/image/download/${roomNo}`, {method: 'GET'}).then(response => {
+        if (response.ok) {
+            return response.blob();
+        }
+        throw new Error('이미지 다운로드 실패');
+    }).then(imageBlob => {
+        console.log(imageBlob)
+        const imageUrl = URL.createObjectURL(imageBlob);
+        const downloadedImage = document.getElementById(roomNo);
+        downloadedImage.src = imageUrl;
+        console.log("성공")
+    }).catch(error => {
+        console.error('이미지 다운로드 실패:', error);
+    });
+
 }
