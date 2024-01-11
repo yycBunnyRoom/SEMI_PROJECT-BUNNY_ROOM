464479cffe9f00af97aa0cf29b655cf9880e2b17
isac7722 <isac7722@gmail.com>
2024-01-11 16:54:36 +0900

    environment

---
+++ bunny-room/src/main/java/com/yyc/bunnyroom/common/config/KeysForYYC.java
@@ -0,0 +1,14 @@
+package com.yyc.bunnyroom.common.config;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Getter
+public class KeysForYYC {
+
+    private String kakaoAPI ="13c9741586ee1ef04079d7c79c9ca27b";
+
+    private String adminEmailAddress = "kjsintellij2312@gmail.com";
+    private String adminGoogleAppPassword = "aofw znqi ykph uzbm";
+
+}
